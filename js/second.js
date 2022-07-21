8a4407a22199fd43169eb925a6f3fc4b06fefaec
dakyun <dkdk_21@naver.com>
2022-07-21 17:23:23 +0900

    20220721 작업

--- js/second.js
+++ js/second.js
@@ -1,37 +1,37 @@
-/* Open the first section on l  oad */
-$(".section-title:first").addClass('open');
-var openOnLoad = $(".section-title:first").next('.collapsing-section');
-$(openOnLoad).slideDown();
-
-$(".section-title").on('focus', function () {
-  if (!$(this).data("mouseDown"))
-    $(this).click();
-});
-
-$(".section-title").on('mousedown', function () {
-  $(this).data("mouseDown", true);
-});
-
-$(".section-title").on('mouseup', function () {
-  $(this).removeData("mouseDown");
-});
-
+// accordian menu
 $(".section-title").on('click', function (e) {
   if ($(this).hasClass('open')) {
-    //Close the current section
     $('.section-title').removeClass('open');
     $('.collapsing-section').slideUp();
   } else {
-    //close the prev section & open the newly click
     $('.section-title').removeClass('open');
-    $('.collapsing-section').slideUp(); //Side up all sections that are open & remove their open class
+    $('.collapsing-section').slideUp();
     $(this).addClass('open');
     var sectionToOpen = $(this).next('.collapsing-section');
     $(sectionToOpen).slideDown();
   }
 });
 
- 
+//slick slider
+$('.reward-banner>ul').slick({
+  arrows:false,
+  dots:false,
+  autoplay:true,
+  autoplaySpeed:3000,
+  infinite:true,
+});
+
+//popup
+function popUp(){
+  $('#name').val('');
+  $('#phone_No').val('');
+  $('#popup').css('display', 'flex').hide().fadeIn();
+}
+
+function modalClose(){
+  $('#popup').fadeOut();
+}
+
 const reply = $('.reply-container>ul>li')
 
 reply.click(function(){
